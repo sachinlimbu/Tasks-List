04882811ed046cc4771eeacdca15ff052e174bc7
Sachin Limbu <sachinlimbu@outlook.com>
2019-08-19 20:14:37 +0100

    Validation  & Winning case

---
+++ NumberGuesser/js/app.js
@@ -0,0 +1,58 @@
+/*
+Game Function:
+- Player must gues a number between a min and max
+- Player gets a certain amount of guesses
+- Notify player of guesses remaining
+- Notify the player of the correct answer if loose
+- Let player choose to play again
+*/
+
+//Game values
+let min  = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+
+// UI Elements
+
+const game = document.querySelector('#game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessInput = document.querySelector('#guess-input'),
+      guessBtn = document.querySelector('#guess-btn'),
+      message = document.querySelector('.message');
+
+//Assign UI min and max
+
+minNum.textContent = min;
+maxNum.textContent = max;
+
+//Listen for guess
+
+guessBtn.addEventListener('click',function(){
+ let guess = parseInt(guessInput.value);
+ //Validate
+  if(isNaN(guess)||guess < min||guess > max){
+    setMessage(`Please enter a number between ${min} and ${max}`,'red');
+  }
+
+  //check if won
+  if(guess === winningNum){
+    //disable input
+    guessInput.disabled = true;
+    //change border color
+    guessInput.style.bordercolor = 'green';
+    //Set Message
+    setMessage(`${winningNum} is correct!`,'Green');
+  }
+});
+
+
+//Set message
+
+function setMessage(msg,color){
+  message.textContent = msg;
+  message.style.color = color;
+}
+
