63ed6fd8981581e90dd52c8946f7e2ce57d36b48
Sachin Limbu <sachinlimbu@outlook.com>
2019-08-19 16:52:10 +0100

    Calculate & Error

---
+++ LoanCalculator/js/app.js
@@ -0,0 +1,74 @@
+//Listen for submit
+
+document.querySelector('#loan-form').addEventListener('submit',calculateResult);
+
+
+//function Calculate Result 
+function calculateResult(e){
+
+  console.log('Calculating.....');
+//UI vars
+const amount = document.querySelector('#amount');
+const interest = document.querySelector('#interest');
+const years = document.querySelector('#years');
+const monthlyPayment = document.querySelector('#monthly-payment');
+const totalPayment = document.querySelector('#total-payment');
+const totalInterest = document.querySelector('#total-interest');
+//Compute Monthly Payment
+
+const principal = parseFloat(amount.value);
+const calculatedInterest = parseFloat(interest.value) /100 / 12;
+const calculatedPayments = parseFloat(years.value) * 12;
+
+//Compute monthly Payment
+const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+
+const monthly = (principal * x * calculatedInterest)/(x-1);
+
+if(isFinite(monthly)){
+  monthlyPayment.value = monthly.toFixed(2);
+  totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+  totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+}else{
+  // console.log('Please check your numbers');
+  showError('Please check your numbers');
+}
+
+
+e.preventDefault();
+}
+
+
+//Show Error
+
+function showError(error){
+
+  //Create a div
+  const errorDiv = document.createElement('div');
+  //Get elemtn
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+
+  //Add class 
+  errorDiv.className = 'alert alert-danger';
+
+  //Create text node and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+  //Insert error above heading
+  card.insertBefore(errorDiv,heading);
+
+  //Clear error after 3 seconds
+  setTimeout(clearError, 3000);
+}
+
+//clear error
+function clearError(){
+  document.querySelector('.alert').remove();
+}
+
+
+
+
+
+
+
